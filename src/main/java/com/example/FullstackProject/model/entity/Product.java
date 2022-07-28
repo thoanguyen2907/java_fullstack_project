a5f26feef5ff226dd85cb2dcfaa07430dcc4bd82
tuanbeovnn <49017369+tuanbeovnn@users.noreply.github.com>
2022-07-28 22:10:22 +0300

    Revert "create one to many relationship"

--- src/main/java/com/example/FullstackProject/model/entity/Product.java
+++ src/main/java/com/example/FullstackProject/model/entity/Product.java
@@ -5,9 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
-
-import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Getter
 @Setter
@@ -15,37 +14,11 @@ import static javax.persistence.GenerationType.SEQUENCE;
 @Entity
 @Table(name = "product")
 public class Product  extends BaseEntity {
-
-    @Id
-    @SequenceGenerator(
-            name = "product_sequence",
-            sequenceName = "product_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = SEQUENCE,
-            generator = "product_sequence"
-    )
-    @Column(
-            name = "id",
-            updatable = false
-    )
-    private Long id;
     private String name;
     private String brand;
+    private String category;
     private String description;
     private int rating;
     private int price;
-    private int category_id;
     private int count_in_stock;
-    @ManyToOne
-    @JoinColumn(
-            name = "category_id",
-            nullable = false,
-            referencedColumnName = "id",
-            foreignKey = @ForeignKey(
-                    name = "product_category_id_fk"
-            )
-    )
-    private Category category;
 }
