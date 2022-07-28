8ad26e263df5589258df2e9e94f6314b31332ce2
thoanguyen2907 <thoanguyen290795@gmail.com>
2022-07-28 21:32:24 +0300

    create one to many relationship

--- src/main/java/com/example/FullstackProject/model/entity/Product.java
+++ src/main/java/com/example/FullstackProject/model/entity/Product.java
@@ -5,8 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 @Getter
 @Setter
@@ -14,11 +15,37 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "product")
 public class Product  extends BaseEntity {
+
+    @Id
+    @SequenceGenerator(
+            name = "product_sequence",
+            sequenceName = "product_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "product_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
     private String name;
     private String brand;
-    private String category;
     private String description;
     private int rating;
     private int price;
+    private int category_id;
     private int count_in_stock;
+    @ManyToOne
+    @JoinColumn(
+            name = "category_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "product_category_id_fk"
+            )
+    )
+    private Category category;
 }
